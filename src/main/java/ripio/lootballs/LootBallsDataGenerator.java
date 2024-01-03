ccd42443a36aa39fc68242edfc3e63852b9d7bca
Francisco Arenas <francis.arenasv@uc.cl>
2024-01-03 16:44:51 -0300

    Removed unused imports

--- src/main/java/ripio/lootballs/LootBallsDataGenerator.java
+++ src/main/java/ripio/lootballs/LootBallsDataGenerator.java
@@ -1,27 +1,13 @@
 package ripio.lootballs;
 
-import com.cobblemon.mod.common.CobblemonItems;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
-import net.minecraft.item.Items;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.context.LootContextTypes;
-import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.SetCountLootFunction;
-import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.registry.RegistryBuilder;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 import ripio.lootballs.datagen.LootBallsLootTableProvider;
 import ripio.lootballs.datagen.LootBallsWorldGenerator;
 import ripio.lootballs.world.LootBallsConfiguredFeatures;
 import ripio.lootballs.world.LootBallsPlacedFeatures;
-
-import java.util.function.BiConsumer;
 
 public class LootBallsDataGenerator implements DataGeneratorEntrypoint {
 
