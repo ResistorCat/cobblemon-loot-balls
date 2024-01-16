5815c03fb999c3fb93495bc181aefe17cac7705b
Francisco <francis.arenasv@uc.cl>
2024-01-16 18:36:56 -0300

    Added functionality to loot balls per chunk config

--- src/main/java/ripio/lootballs/world/LootBallsPlacement.java
+++ src/main/java/ripio/lootballs/world/LootBallsPlacement.java
@@ -1,11 +1,16 @@
 package ripio.lootballs.world;
 
 import net.minecraft.world.gen.placementmodifier.*;
+import ripio.lootballs.config.LootBallsConfigs;
 
 import java.util.List;
 
 public class LootBallsPlacement {
     public static List<PlacementModifier> modifiers() {
-        return List.of(SquarePlacementModifier.of(), BiomePlacementModifier.of());
+        return List.of(
+                SquarePlacementModifier.of(),
+                BiomePlacementModifier.of(),
+                CountPlacementModifier.of(LootBallsConfigs.MAX_LOOTBALLS_PER_CHUNK)
+        );
     }
 }
