f72165c466e57a822ca1c0432823116c8d7fd819
Francisco Arenas <francis.arenasv@uc.cl>
2024-02-03 02:37:58 -0300

    Removed unused doLootBallFishing gamerule

--- src/main/java/ripio/lootballs/world/LootBallsGamerules.java
+++ src/main/java/ripio/lootballs/world/LootBallsGamerules.java
@@ -3,16 +3,13 @@ package ripio.lootballs.world;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
-import ripio.lootballs.Lootballs;
+import ripio.lootballs.LootBalls;
 
 public class LootBallsGamerules {
     public static final GameRules.Key<GameRules.BooleanRule> GENERATE_LOOT_BALLS = GameRuleRegistry.register(
             "generateLootBalls", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true)
     );
-    public static final GameRules.Key<GameRules.BooleanRule> DO_LOOT_BALL_FISHING = GameRuleRegistry.register(
-            "doLootBallFishing", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true)
-    );
     public static void registerGamerules() {
-        Lootballs.LOGGER.info("Registering gamerules for " + Lootballs.MOD_ID);
+        LootBalls.LOGGER.info("Registering gamerules for " + LootBalls.MOD_ID);
     }
 }
