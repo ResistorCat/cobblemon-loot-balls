f53d591de36066dda3cb8dd3db935c59ae322874
Francisco Arenas <111025583+ResistorCat@users.noreply.github.com>
2024-10-09 00:04:35 -0300

    Register loot ball item renderer

--- src/client/java/ripio/lootballs/LootBallsClient.java
+++ src/client/java/ripio/lootballs/LootBallsClient.java
@@ -1,17 +1,21 @@
 package ripio.lootballs;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
-import ripio.lootballs.block.entity.LootBallEntityRenderer;
+import ripio.lootballs.render.block.LootBallEntityRenderer;
 import ripio.lootballs.model.LootBallModel;
+import ripio.lootballs.render.item.LootBallItemRenderer;
 
 import static ripio.lootballs.block.LootBallsBlockEntities.LOOT_BALL_ENTITY;
+import static ripio.lootballs.block.LootBallsBlocks.LOOT_BALL;
 
 public class LootBallsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		EntityModelLayerRegistry.registerModelLayer(LootBallModel.LAYER_LOCATION, LootBallModel::getTexturedModelData);
 		BlockEntityRendererFactories.register(LOOT_BALL_ENTITY, LootBallEntityRenderer::new);
+		BuiltinItemRendererRegistry.INSTANCE.register(LOOT_BALL, new LootBallItemRenderer());
 	}
 }
